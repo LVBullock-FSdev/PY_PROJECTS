8314db555bb881a0b6ffd08e85b268b9b9925026
Laura V. Bullock <laura.v.bullock@gmail.com>
2024-09-20 18:47:06 -0400

    modified code to use f-string to print decimal places vs round method

--- body_weight_convertor.py
+++ body_weight_convertor.py
@@ -11,6 +11,6 @@ Write a program that will ask for a user to enter his/her body weight in pounds 
 Display on the screen: your body weight is weight in lbs, and the equivalent kgs is weight kg.  Thank you!  only display three digit decimal e.g:10.234'''
 
 weight_in_lbs = float(input("Please enter your body weight in pounds (lbs):\t"))
-weight_kg  = round((weight_in_lbs * 0.453592),3)
+weight_kg  = (weight_in_lbs * 0.453592)
 
-print(f"Your body weight is {weight_in_lbs} in lbs, and the equivalent kgs is {weight_kg}.  Thank you!")
+print(f"Your body weight is {weight_in_lbs} in lbs, and the equivalent kgs is {weight_kg:0,.3f}.  Thank you!")
