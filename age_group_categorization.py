46cdfb785489154091c5e1bd755365d23375f1a1
Laura V. Bullock <laura.v.bullock@gmail.com>
2024-10-01 20:54:40 -0400

    Updating code with corrections.

--- age_group_categorization.py
+++ age_group_categorization.py
@@ -13,25 +13,27 @@ invalid_msg = f"The age entered is:  {age}.  Invalid age! You have failed the hu
 #Based on the input, categorize the person into one of the following life stages:
 
 #Invalid entry
-if age < 0 or age > 150:
-  print(invalid_msg)
+# if age < 0 or age > 150:
+#   print(invalid_msg)
 #Infant: 0-1 year
-elif age == 0 or age == 1:
+if age == 0 or age == 1:
   print(age_msg + "INFANT")
 #Toddler: 2-3 years
 elif age == 2 or age == 3:
   print(age_msg + "TODDLER")
 #Child: 4-12 years
-elif age == 4 or age < 13:
+elif age >= 4 and age <= 12:
+
+# elif age == 4 or age < 13:
   print(age_msg + "CHILD")
 #Teenager: 13-19 years
-elif age == 13 or age < 20:
+elif age >= 13 and age <= 19:
   print(age_msg + "TEENAGER")
 #Adult: 20-64 years
-elif age == 20 or age < 65:
+elif age >= 20 and age <= 64:
   print(age_msg + "ADULT")
 #Senior: 65 years and older
-elif age == 65 or age < 151:
+elif age >= 65 and age <= 150:
   print(age_msg + "SENIOR")
 else:
   print(invalid_msg)
