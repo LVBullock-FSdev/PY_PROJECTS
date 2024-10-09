f6f48cf7b78a4f27a3dc874e29dda64c66172489
Laura V. Bullock <laura.v.bullock@gmail.com>
2024-10-08 20:03:12 -0400

    Modified

--- get_min_max.py
+++ get_min_max.py
@@ -16,12 +16,16 @@ Expected output:
 []              --> []
 '''
 
-#my_list = [3,4,5,6]
-#my_list = [-1,-2,-3,-4]
-#my_list = [0,0,0,0]
-my_list = [] 
+my_list1 = [3,4,5,6]
+my_list2 = [-1,-2,-3,-4]
+my_list3 = [0,0,0,0]
+my_list4 = [] 
 
-if len(my_list) != 0:
-    print(max(my_list), min(my_list))
-else:
-    print(my_list, "None")
+if len(my_list1) != 0:
+    print(max(my_list1), min(my_list1))
+if len(my_list2) != 0:
+    print(max(my_list2), min(my_list2))
+if len(my_list3) != 0:
+    print(max(my_list3), min(my_list3))
+
+print(my_list4, "None")
