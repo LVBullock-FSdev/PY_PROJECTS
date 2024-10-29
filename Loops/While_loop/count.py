5fa1fce62ec7519116728f0e41ddedba0afb6b34
Laura V. Bullock <laura.v.bullock@gmail.com>
2024-10-29 16:04:27 -0400

    Small change.

--- Loops/While_loop/count.py
+++ Loops/While_loop/count.py
@@ -10,7 +10,7 @@ print(f"\nPrinting from 1 to 10...")
 #print 1 to 10
 while count1 <= 10:
     print(count1)
-    count1 = count1 + 1
+    count1 += 1
 
 '''Write a while-loop and inside of your while-loop count down backward from 10 to 0.
 Print "Starting to countdown backward from 10-0" at the beginning
@@ -22,6 +22,6 @@ print(f"\nStarting to countdown backward from 10 to 0....")
 
 while count2 > 0:
     print(count2)
-    count2 = count2 - 1
+    count2 -= 1
 
 print("\n\tCOUNTDOWN IS OVER!")
